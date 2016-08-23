3d45396d200ae6da959ed872d48e2db1d71b30a6
jgvictores <jcgvicto@gmail.com>
2016-08-23 13:09:38 +0200

    updating the project

---
+++ programs/teoCartesianServer/TeoXCallbackPort.cpp
@@ -0,0 +1,16 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:4; indent-tabs-mode:nil -*-
+
+#include "TeoXCallbackPort.hpp"
+
+namespace teo
+{
+
+/************************************************************************/
+
+void TeoXCallbackPort::onRead(yarp::os::Bottle& b) {
+    printf("[CartesianServerLib] xCallbackPort Got %s\n", b.toString().c_str());
+}
+
+/************************************************************************/
+
+}  // namespace teo
