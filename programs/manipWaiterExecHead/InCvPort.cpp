a59dc924c754ee8c63cbf8092796497d097e2fd2
jgvictores <jcgvicto@gmail.com>
2016-10-26 12:21:17 +0200

    adding control head

--- programs/manipWaiterExecHead/InCvPort.cpp
+++ programs/manipWaiterExecHead/InCvPort.cpp
@@ -24,56 +24,56 @@ void InCvPort::onRead(Bottle& b) {
 
     printf("%f %f %f\n",x,y,z);
 
-    if( x > 385 ) {
-        iVelocityControl->velocityMove(0, -6.0); //Id motor. Velocity [deg/s].
+    if( x >= 385 ) {
+        iVelocityControl->velocityMove(0, -6); //Id motor. Velocity [deg/s].
     }
-    else if( x > 365 && x < 385 ) {
-        iVelocityControl->velocityMove(0, -4.0);
+    else if( x >= 365 && x < 385 ) {
+        iVelocityControl->velocityMove(0, -4);
     }
-    else if( x > 345 && x < 365 ) {
-        iVelocityControl->velocityMove(0, -2.0);
+    else if( x >= 345 && x < 365 ) {
+        iVelocityControl->velocityMove(0, -2);
     }
-    else if( x > 325 && x < 345 ) {
-        iVelocityControl->velocityMove(0, -1.0);
+    else if( x >= 325 && x < 345 ) {
+        iVelocityControl->velocityMove(0, -1);
     }
     else if( x > 315 && x < 325 ) {
-        iVelocityControl->velocityMove(0, 0.0);
+        iVelocityControl->velocityMove(0, 0);
     }
-    else if( x < 315 && x > 295 ) {
-        iVelocityControl->velocityMove(0, 1.0);
+    else if( x <= 315 && x > 295 ) {
+        iVelocityControl->velocityMove(0, 1);
     }
-    else if( x < 295 && x > 275 ) {
-        iVelocityControl->velocityMove(0, 2.0);
+    else if( x <= 295 && x > 275 ) {
+        iVelocityControl->velocityMove(0, 2);
     }
-    else if( x < 275 && x > 255 ) {
-        iVelocityControl->velocityMove(0, 4.0);
+    else if( x <= 275 && x > 255 ) {
+        iVelocityControl->velocityMove(0, 4);
     }
-    else if( x < 255 ) {
-        iVelocityControl->velocityMove(0, 6.0);
+    else if( x <= 255 ) {
+        iVelocityControl->velocityMove(0, 6);
     }
 
 
 
-    if( y > 185 ) {
-        iVelocityControl->velocityMove(1, 3.0); //Id motor. Velocity [deg/s].
+    if( y >= 185 ) {
+        iVelocityControl->velocityMove(1, 3); //Id motor. Velocity [deg/s].
     }
-    else if( y > 165 && y < 185) {
-        iVelocityControl->velocityMove(1, 2.0);
+    else if( y >= 165 && y < 185) {
+        iVelocityControl->velocityMove(1, 2);
     }
-    else if( y > 145 && y < 165 ) {
-        iVelocityControl->velocityMove(1, 1.0);
+    else if( y >= 145 && y < 165 ) {
+        iVelocityControl->velocityMove(1, 1);
     }
     else if( y > 135 && y < 145 ) {
-        iVelocityControl->velocityMove(1, 0.0);
+        iVelocityControl->velocityMove(1, 0);
     }
-    else if( y < 135 && y > 115 ) {
-        iVelocityControl->velocityMove(1, -1.0);
+    else if( y <= 135 && y > 115 ) {
+        iVelocityControl->velocityMove(1, -1);
     }
-    else if( y < 115 && y > 95 ) {
-        iVelocityControl->velocityMove(1, -2.0);
+    else if( y <= 115 && y > 95 ) {
+        iVelocityControl->velocityMove(1, -2);
     }
-    else if( y < 95 ) {
-        iVelocityControl->velocityMove(1, -3.0);
+    else if( y <= 95 ) {
+        iVelocityControl->velocityMove(1, -3);
     }
 
 }
