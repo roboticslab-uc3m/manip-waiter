4a32d71bed42ebae83a9839a42b5bd8ee48b6996
jgvictores <jcgvicto@gmail.com>
2016-10-27 17:31:10 +0200

    new control head

--- programs/manipWaiterExecManip/InCvPort.cpp
+++ programs/manipWaiterExecManip/InCvPort.cpp
@@ -45,13 +45,13 @@ void InCvPort::onRead(Bottle& b) {
         iteration=1;
         yarp::os::Time::delay(2);
 
-/*        while( ! done )
+       while( ! done )
         {
             yarp::os::Time::delay(2);
-//            iPositionControl->checkMotionDone(&done);
+            iPositionControl->checkMotionDone(&done);
             printf(".");
             fflush(stdout);
-        }*/
+        }
 
         a=1;
     }
